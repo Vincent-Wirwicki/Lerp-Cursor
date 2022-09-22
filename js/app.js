d7c76107112e93bec2be04ebba004f9b57eef2b4
Vincent <98763680+Vincent-Wirwicki@users.noreply.github.com>
2022-09-22 17:09:43 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,11 +1,10 @@
-const cursor = document.querySelector(".cursor");
-//cursor speed
+const cursors = [...document.querySelectorAll(".cursor")];
 const ease = 0.075;
-let updateX, updateY;
-let mouseX,
-  cursorX = window.innerWidth / 2;
-let mouseY,
-  cursorY = window.innerHeight / 2;
+
+let mouseX = 0,
+  mouseY = 0,
+  cursorX = 0,
+  cursorY = 0;
 
 const lerp = (start, end, ease) => (1 - ease) * start + ease * end;
 
@@ -15,12 +14,13 @@ const getMousePos = e => {
 };
 
 const animateCursor = () => {
-  const { height, width } = cursor.getBoundingClientRect();
-  updateX = parseFloat((mouseX - width / 2).toFixed(2));
-  updateY = parseFloat((mouseY - height / 2).toFixed(2));
-  cursorX = lerp(cursorX, mouseX, ease);
-  cursorY = lerp(cursorY, mouseY, ease);
-  cursor.style.transform = `translate3d(${updateX}px, ${updateY}px, 0)`;
+  cursors.forEach(cursor => {
+    const { height, width } = cursor.getBoundingClientRect();
+    cursorX = lerp(cursorX, mouseX, ease);
+    cursorY = lerp(cursorY, mouseY, ease);
+    cursor.style.transform = `translate3d(${cursorX - width / 2}px, 
+    ${cursorY - height / 2}px, 0)`;
+  });
 };
 
 const raf = () => {
