44e674b4e793ad8ae8916909103687e9c6809674
Vinz W <vincent.wirwicki@gmail.com>
2022-10-18 00:51:14 +0200

    add hover scale  effect

---
+++ js/test.js
@@ -0,0 +1,17 @@
+// if you wanna change more stuff inside cursorsOnHover
+// if (isHover) {
+//   cursorsHeightWidth(cursors[0], cursorsDimensions[1] * scale);
+//   cssAddRemoveClass(cursors[0], "bakground_white", "background__black");
+//   cursorsHeightWidth(cursors[1], cursorsDimensions[0]);
+//   cssAddRemoveClass(cursors[1], "border__black", "border__white");
+// } else {
+//   cursorsHeightWidth(cursors[0], cursorsDimensions[0]);
+//   cssAddRemoveClass(cursors[0], "bakground_white", "background__black");
+//   cursorsHeightWidth(cursors[1], cursorsDimensions[1]);
+//   cssAddRemoveClass(cursors[1], "border__white", "border__black");
+// }
+
+// const cssAddRemoveClass = (element, add, remove) => {
+//   element.classList.add(add);
+//   element.classList.remove(remove);
+// };
